2b0087d80456435096e9ac29eeeed7b68156141a
Jesse Kane <jessekane66@gmail.com>
2024-06-27 20:44:15 -0400

    added instance variables and a few getter functions

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -7,27 +7,48 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class Vision {
 
-
     public String limeString;
+    private double tx, ty, ta;
+    private int apriltags;
+    private int retroReflective;
+    private double minTargetAspectRationReflective, maxTargetAspectRationReflective;
+    private double minTargetAspectRationAprilTag, maxTargetAspectRationAprilTag;
+    private boolean shouldUpdatePose;
+
+    private static final double conversionScalar = 39.37;
+    private static final String defaultLimeString = "limelight";
 
     public Vision(String _limelightString) {
         this.limeString = _limelightString;
     }
+
+    public Vision() {this.limeString = defaultLimeString;}
     
     public void periodic(){
         NetworkTable table = NetworkTableInstance.getDefault().getTable(this.limeString);
-        NetworkTableEntry tx = table.getEntry("tx");
-        NetworkTableEntry ty = table.getEntry("ty");
-        NetworkTableEntry ta = table.getEntry("ta");
-
-        //get values periodically
-        double x = tx.getDouble(0.0);
-        double y = ty.getDouble(0.0);
-        double area = ta.getDouble(0.0);
+        tx = table.getEntry("tx").getDouble(0);
+        ty = table.getEntry("ty").getDouble(0);
+        ta = table.getEntry("ta").getDouble(0);
 
         //post to smart dashboard periodically
-        SmartDashboard.putNumber("LimelightX", x);
-        SmartDashboard.putNumber("LimelightY", y);
-        SmartDashboard.putNumber("LimelightArea", area);
+        SmartDashboard.putNumber("LimelightX", tx);
+        SmartDashboard.putNumber("LimelightY", ty);
+        SmartDashboard.putNumber("LimelightArea", ta);
+    }
+
+    public double[] getPose(){
+        return NetworkTableInstance.getDefault().getTable(limeString).getEntry("botpose").getDoubleArray(new double[6]);
+    }
+
+    public double getTx(){
+        return tx;
+    }
+
+    public double getTy(){
+        return ty;
+    }
+
+    public double getTa(){
+        return ta;
     }
 }
